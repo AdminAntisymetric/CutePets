780dcce333172ad04ea27245d3b527ebe9a86b09
joaquinrot5 <joaquin.rt@live.com>
2016-02-22 20:07:23 -0600

    se sube prefab y scripts para agregar boton de pausa
    se sube prefab y scripts para agregar boton de pausa usar con build en
    android

---
+++ CutePets/Assets/Scripts/TouchControls.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchControls : MonoBehaviour 
+{
+	private PauseMenu thePauseMenu;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		thePauseMenu = FindObjectOfType<PauseMenu> ();
+	}
+	public void Pause()
+	{
+		thePauseMenu.pauseUnpause ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
